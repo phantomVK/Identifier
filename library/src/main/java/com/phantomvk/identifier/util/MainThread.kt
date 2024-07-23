85c81ef98ca046b4250d7f03f966c7c0da5badd0
tanwenkang <tanwenkang@kuaishou.com>
2024-07-24 02:22:32 +0800

    Create a new main thread handler in the method.

--- library/src/main/java/com/phantomvk/identifier/util/MainThread.kt
+++ library/src/main/java/com/phantomvk/identifier/util/MainThread.kt
@@ -3,12 +3,14 @@ package com.phantomvk.identifier.util
 import android.os.Handler
 import android.os.Looper
 
-private val mainHandler = Handler(Looper.getMainLooper())
-
 fun runOnMainThread(delayMillis: Long = 0, runnable: Runnable) {
   if (Looper.getMainLooper() == Looper.myLooper()) {
     runnable.run()
-  } else if (delayMillis <= 0L) {
+    return
+  }
+
+  val mainHandler = Handler(Looper.getMainLooper())
+  if (delayMillis <= 0L) {
     mainHandler.post(runnable)
   } else {
     mainHandler.postDelayed(runnable, delayMillis)
