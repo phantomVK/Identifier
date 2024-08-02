bacb6fa0443bc46b2efdbf23ce0ba818421c683a
tanwenkang <tanwenkang@kuaishou.com>
2024-08-03 00:34:01 +0800

    Reuse the static MainHandler.

--- library/src/main/java/com/phantomvk/identifier/impl/Thread.kt
+++ library/src/main/java/com/phantomvk/identifier/impl/Thread.kt
@@ -3,13 +3,14 @@ package com.phantomvk.identifier.impl
 import android.os.Handler
 import android.os.Looper
 
+private val mainHandler = Handler(Looper.getMainLooper())
+
 fun runOnMainThread(delayMillis: Long = 0, runnable: Runnable) {
   if (Looper.getMainLooper() == Looper.myLooper()) {
     runnable.run()
     return
   }
 
-  val mainHandler = Handler(Looper.getMainLooper())
   if (delayMillis <= 0L) {
     mainHandler.post(runnable)
   } else {
