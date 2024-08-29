ba5a9afdbbeda61663d1d83431de49b23e88c257
tanwenkang <tanwenkang@kuaishou.com>
2024-08-30 02:14:31 +0800

    New version: 0.4.7: Replace OppoOpenId whit method.

--- library/src/main/java/com/phantomvk/identifier/provider/OppoHeyTapProvider.kt
+++ library/src/main/java/com/phantomvk/identifier/provider/OppoHeyTapProvider.kt
@@ -3,9 +3,7 @@ package com.phantomvk.identifier.provider
 import android.content.ComponentName
 import android.content.Intent
 import android.os.IBinder
-import android.os.IInterface
 import android.os.Parcel
-import android.os.RemoteException
 import com.phantomvk.identifier.model.ProviderConfig
 
 internal open class OppoHeyTapProvider(config: ProviderConfig) : AbstractProvider(config) {
@@ -24,53 +22,36 @@ internal open class OppoHeyTapProvider(config: ProviderConfig) : AbstractProvide
   }
 
   protected fun getId(descriptor: String, pkg: String, cla: String, action: String) {
+    val component = ComponentName(pkg, cla)
+    val intent = Intent(action).setComponent(component)
     val binderCallback = object : BinderCallback {
       override fun call(binder: IBinder): CallBinderResult {
-        val asInterface = OppoOpenId(binder, descriptor)
-//        if (asInterface == null) {
-//          return CallBinderResult.Failed(AIDL_INTERFACE_IS_NULL)
-//        }
-
+        val id: String
+        val data = Parcel.obtain()
+        val reply = Parcel.obtain()
         val sign = when (val result = getSignatureHash()) {
           is CallBinderResult.Failed -> return result
           is CallBinderResult.Success -> result.id
         }
 
-        val id = asInterface.getSerID(config.context.packageName, sign, "OUID")
+        try {
+          data.writeInterfaceToken(descriptor)
+          data.writeString(config.context.packageName)
+          data.writeString(sign)
+          data.writeString("OUID")
+          binder.transact(1, data, reply, 0)
+          reply.readException()
+          id = reply.readString()!!
+        } finally {
+          reply.recycle()
+          data.recycle()
+        }
+
         return checkId(id)
       }
     }
 
-    val component = ComponentName(pkg, cla)
-    val intent = Intent(action).setComponent(component)
     bindService(intent, binderCallback)
-  }
-
-  private class OppoOpenId(
-    private val remote: IBinder,
-    private val descriptor: String
-  ) : IInterface {
-    override fun asBinder(): IBinder { return remote }
-
-    @Throws(RemoteException::class)
-    fun getSerID(pkgName: String, sign: String, type: String): String {
-      val data = Parcel.obtain()
-      val reply = Parcel.obtain()
-      val result: String
-      try {
-        data.writeInterfaceToken(descriptor)
-        data.writeString(pkgName)
-        data.writeString(sign)
-        data.writeString(type)
-        remote.transact(1, data, reply, 0)
-        reply.readException()
-        result = reply.readString()!!
-      } finally {
-        reply.recycle()
-        data.recycle()
-      }
-      return result
-    }
   }
 
 //  private fun getIdName(name: String): String {
