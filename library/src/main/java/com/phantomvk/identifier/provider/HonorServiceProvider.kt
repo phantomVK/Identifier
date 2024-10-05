3dd0937246f927771cabfda1a27cdccf66a86890
tanwenkang <tanwenkang@kuaishou.com>
2024-10-06 02:57:24 +0800

    Removed class: IOAIDService.java

--- library/src/main/java/com/phantomvk/identifier/provider/HonorServiceProvider.kt
+++ library/src/main/java/com/phantomvk/identifier/provider/HonorServiceProvider.kt
@@ -3,9 +3,9 @@ package com.phantomvk.identifier.provider
 import android.content.Intent
 import android.os.Bundle
 import android.os.IBinder
+import android.os.Parcel
 import com.phantomvk.identifier.model.ProviderConfig
 import generated.com.hihonor.cloudservice.oaid.IOAIDCallBack
-import generated.com.hihonor.cloudservice.oaid.IOAIDService
 import java.util.concurrent.CountDownLatch
 
 internal class HonorServiceProvider(config: ProviderConfig) : AbstractProvider(config) {
@@ -17,19 +17,14 @@ internal class HonorServiceProvider(config: ProviderConfig) : AbstractProvider(c
   override fun run() {
     val binderCallback = object : BinderCallback {
       override fun call(binder: IBinder): CallBinderResult {
-        val asInterface = IOAIDService.Stub.asInterface(binder)
-        if (asInterface == null) {
-          return CallBinderResult.Failed(AIDL_INTERFACE_IS_NULL)
-        }
-
         if (config.isLimitAdTracking) {
-          val result = isLimited(asInterface)
+          val result = isLimited(binder)
           if (result != null) {
             return result
           }
         }
 
-        return getId(asInterface) ?: CallBinderResult.Failed(ID_INFO_IS_NULL)
+        return getId(binder) ?: CallBinderResult.Failed(ID_INFO_IS_NULL)
       }
     }
 
@@ -38,27 +33,12 @@ internal class HonorServiceProvider(config: ProviderConfig) : AbstractProvider(c
     bindService(intent, binderCallback)
   }
 
-  private fun isLimited(asInterface: IOAIDService): CallBinderResult? {
+  private fun getId(iBinder: IBinder): CallBinderResult? {
     var result: CallBinderResult? = null
     val latch = CountDownLatch(1)
-    asInterface.isLimited(object : IOAIDCallBack.Stub() {
-      override fun a(i: Int, j: Long, z: Boolean, f: Float, d: Double, str: String?) {}
-      override fun onResult(i: Int, bundle: Bundle?) {
-        if (i == 0 && bundle?.getBoolean("oa_id_limit_state") == true) {
-          result = CallBinderResult.Failed(LIMIT_AD_TRACKING_IS_ENABLED)
-        }
-        latch.countDown()
-      }
-    })
-
-    latch.await()
-    return result
-  }
-
-  private fun getId(asInterface: IOAIDService): CallBinderResult? {
-    var result: CallBinderResult? = null
-    val latch = CountDownLatch(1)
-    asInterface.getOaid(object : IOAIDCallBack.Stub() {
+    val data = Parcel.obtain()
+    val reply = Parcel.obtain()
+    val callback = object : IOAIDCallBack.Stub() {
       override fun a(i: Int, j: Long, z: Boolean, f: Float, d: Double, str: String?) {}
       override fun onResult(i: Int, bundle: Bundle?) {
         if (i != 0 || bundle == null) {
@@ -70,7 +50,46 @@ internal class HonorServiceProvider(config: ProviderConfig) : AbstractProvider(c
         result = checkId(bundle.getString("oa_id_flag"))
         latch.countDown()
       }
-    })
+    }
+
+    try {
+      data.writeInterfaceToken("com.hihonor.cloudservice.oaid.IOAIDService")
+      data.writeStrongInterface(callback)
+      iBinder.transact(2, data, reply, 0)
+      reply.readException()
+    } finally {
+      reply.recycle()
+      data.recycle()
+    }
+
+    latch.await()
+    return result
+  }
+
+  private fun isLimited(iBinder: IBinder): CallBinderResult? {
+    var result: CallBinderResult? = null
+    val latch = CountDownLatch(1)
+    val data = Parcel.obtain()
+    val reply = Parcel.obtain()
+    val callback = object : IOAIDCallBack.Stub() {
+      override fun a(i: Int, j: Long, z: Boolean, f: Float, d: Double, str: String?) {}
+      override fun onResult(i: Int, bundle: Bundle?) {
+        if (i == 0 && bundle?.getBoolean("oa_id_limit_state") == true) {
+          result = CallBinderResult.Failed(LIMIT_AD_TRACKING_IS_ENABLED)
+        }
+        latch.countDown()
+      }
+    }
+
+    try {
+      data.writeInterfaceToken("com.hihonor.cloudservice.oaid.IOAIDService")
+      data.writeStrongInterface(callback)
+      iBinder.transact(3, data, reply, 0)
+      reply.readException()
+    } finally {
+      reply.recycle()
+      data.recycle()
+    }
 
     latch.await()
     return result
