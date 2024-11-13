29936cc5298ee5756605727742629a0765842762
tanwenkang <tanwenkang@kuaishou.com>
2024-11-13 13:10:48 +0800

    Optimize memory cache system.

---
+++ library/src/main/java/com/phantomvk/identifier/impl/CacheCenter.kt
@@ -0,0 +1,19 @@
+package com.phantomvk.identifier.impl
+
+import com.phantomvk.identifier.model.IdentifierResult
+import com.phantomvk.identifier.model.ProviderConfig
+import java.util.concurrent.ConcurrentHashMap
+
+internal object CacheCenter {
+  private val map = ConcurrentHashMap<String, IdentifierResult>()
+
+  fun get(config: ProviderConfig): IdentifierResult? {
+    return if (config.isMemCacheEnabled) map[config.getCacheKey()] else null
+  }
+
+  fun putIfAbsent(config: ProviderConfig, result: IdentifierResult) {
+    if (config.isMemCacheEnabled) {
+      map.putIfAbsent(config.getCacheKey(), result)
+    }
+  }
+}
