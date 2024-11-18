24d9fdb3a0b13159f318befe889b121ec152025a
tanwenkang <tanwenkang@kuaishou.com>
2024-11-18 12:21:42 +0800

    shrink code com.hihonor.cloudservice.oaid.IOAIDCallBack

--- library/src/main/java/generated/com/hihonor/cloudservice/oaid/IOAIDCallBack.java
+++ library/src/main/java/generated/com/hihonor/cloudservice/oaid/IOAIDCallBack.java
@@ -2,30 +2,18 @@
  * This file is auto-generated.  DO NOT MODIFY.
  */
 package generated.com.hihonor.cloudservice.oaid;
+
+import android.os.Parcel;
+
 public interface IOAIDCallBack extends android.os.IInterface
 {
   /** Local-side IPC implementation stub class. */
-  public static abstract class Stub extends android.os.Binder implements generated.com.hihonor.cloudservice.oaid.IOAIDCallBack
+  abstract class Stub extends android.os.Binder implements generated.com.hihonor.cloudservice.oaid.IOAIDCallBack
   {
     /** Construct the stub at attach it to the interface. */
     public Stub()
     {
       this.attachInterface(this, DESCRIPTOR);
-    }
-    /**
-     * Cast an IBinder object into an generated.com.hihonor.cloudservice.oaid.IOAIDCallBack interface,
-     * generating a proxy if needed.
-     */
-    public static generated.com.hihonor.cloudservice.oaid.IOAIDCallBack asInterface(android.os.IBinder obj)
-    {
-      if ((obj==null)) {
-        return null;
-      }
-      android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
-      if (((iin!=null)&&(iin instanceof generated.com.hihonor.cloudservice.oaid.IOAIDCallBack))) {
-        return ((generated.com.hihonor.cloudservice.oaid.IOAIDCallBack)iin);
-      }
-      return new generated.com.hihonor.cloudservice.oaid.IOAIDCallBack.Stub.Proxy(obj);
     }
     @Override public android.os.IBinder asBinder()
     {
@@ -37,43 +25,31 @@ public interface IOAIDCallBack extends android.os.IInterface
       if (code >= android.os.IBinder.FIRST_CALL_TRANSACTION && code <= android.os.IBinder.LAST_CALL_TRANSACTION) {
         data.enforceInterface(descriptor);
       }
-      switch (code)
-      {
-        case INTERFACE_TRANSACTION:
-        {
-          reply.writeString(descriptor);
-          return true;
-        }
+      if (code == INTERFACE_TRANSACTION) {
+        reply.writeString(descriptor);
+        return true;
       }
       switch (code)
       {
         case TRANSACTION_a:
         {
-          int _arg0;
-          _arg0 = data.readInt();
-          long _arg1;
-          _arg1 = data.readLong();
-          boolean _arg2;
-          _arg2 = (0!=data.readInt());
-          float _arg3;
-          _arg3 = data.readFloat();
-          double _arg4;
-          _arg4 = data.readDouble();
-          java.lang.String _arg5;
-          _arg5 = data.readString();
+          int _arg0 = data.readInt();
+          long _arg1 = data.readLong();
+          boolean _arg2 = (0 != data.readInt());
+          float _arg3 = data.readFloat();
+          double _arg4 = data.readDouble();
+          java.lang.String _arg5 = data.readString();
           this.a(_arg0, _arg1, _arg2, _arg3, _arg4, _arg5);
           reply.writeNoException();
           break;
         }
         case TRANSACTION_onResult:
         {
-          int _arg0;
-          _arg0 = data.readInt();
-          android.os.Bundle _arg1;
-          _arg1 = _Parcel.readTypedObject(data, android.os.Bundle.CREATOR);
+          int _arg0 = data.readInt();
+          android.os.Bundle _arg1 = _Parcel.readTypedObject(data);
           this.onResult(_arg0, _arg1);
           reply.writeNoException();
-          _Parcel.writeTypedObject(reply, _arg1, android.os.Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+          _Parcel.writeTypedObject(reply, _arg1);
           break;
         }
         default:
@@ -83,83 +59,25 @@ public interface IOAIDCallBack extends android.os.IInterface
       }
       return true;
     }
-    private static class Proxy implements generated.com.hihonor.cloudservice.oaid.IOAIDCallBack
-    {
-      private android.os.IBinder mRemote;
-      Proxy(android.os.IBinder remote)
-      {
-        mRemote = remote;
-      }
-      @Override public android.os.IBinder asBinder()
-      {
-        return mRemote;
-      }
-      public java.lang.String getInterfaceDescriptor()
-      {
-        return DESCRIPTOR;
-      }
-      @Override public void a(int i, long j, boolean z, float f, double d, java.lang.String str) throws android.os.RemoteException
-      {
-        android.os.Parcel _data = android.os.Parcel.obtain();
-        android.os.Parcel _reply = android.os.Parcel.obtain();
-        try {
-          _data.writeInterfaceToken(DESCRIPTOR);
-          _data.writeInt(i);
-          _data.writeLong(j);
-          _data.writeInt(((z)?(1):(0)));
-          _data.writeFloat(f);
-          _data.writeDouble(d);
-          _data.writeString(str);
-          boolean _status = mRemote.transact(Stub.TRANSACTION_a, _data, _reply, 0);
-          _reply.readException();
-        }
-        finally {
-          _reply.recycle();
-          _data.recycle();
-        }
-      }
-      @Override public void onResult(int i, android.os.Bundle bundle) throws android.os.RemoteException
-      {
-        android.os.Parcel _data = android.os.Parcel.obtain();
-        android.os.Parcel _reply = android.os.Parcel.obtain();
-        try {
-          _data.writeInterfaceToken(DESCRIPTOR);
-          _data.writeInt(i);
-          _Parcel.writeTypedObject(_data, bundle, 0);
-          boolean _status = mRemote.transact(Stub.TRANSACTION_onResult, _data, _reply, 0);
-          _reply.readException();
-          if ((0!=_reply.readInt())) {
-            bundle.readFromParcel(_reply);
-          }
-        }
-        finally {
-          _reply.recycle();
-          _data.recycle();
-        }
-      }
-    }
-    static final int TRANSACTION_a = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+    static final int TRANSACTION_a = android.os.IBinder.FIRST_CALL_TRANSACTION;
     static final int TRANSACTION_onResult = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
   }
-  public static final java.lang.String DESCRIPTOR = "com.hihonor.cloudservice.oaid.IOAIDCallBack";
-  public void a(int i, long j, boolean z, float f, double d, java.lang.String str) throws android.os.RemoteException;
-  public void onResult(int i, android.os.Bundle bundle) throws android.os.RemoteException;
+  java.lang.String DESCRIPTOR = "com.hihonor.cloudservice.oaid.IOAIDCallBack";
+  void a(int i, long j, boolean z, float f, double d, java.lang.String str) throws android.os.RemoteException;
+  void onResult(int i, android.os.Bundle bundle) throws android.os.RemoteException;
   /** @hide */
-  static class _Parcel {
-    static private <T> T readTypedObject(
-        android.os.Parcel parcel,
-        android.os.Parcelable.Creator<T> c) {
+  class _Parcel {
+    static private <T> T readTypedObject(Parcel parcel) {
       if (parcel.readInt() != 0) {
-          return c.createFromParcel(parcel);
+          return ((android.os.Parcelable.Creator<T>) android.os.Bundle.CREATOR).createFromParcel(parcel);
       } else {
           return null;
       }
     }
-    static private <T extends android.os.Parcelable> void writeTypedObject(
-        android.os.Parcel parcel, T value, int parcelableFlags) {
+    static private <T extends android.os.Parcelable> void writeTypedObject(Parcel parcel, T value) {
       if (value != null) {
         parcel.writeInt(1);
-        value.writeToParcel(parcel, parcelableFlags);
+        value.writeToParcel(parcel, android.os.Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
       } else {
         parcel.writeInt(0);
       }
