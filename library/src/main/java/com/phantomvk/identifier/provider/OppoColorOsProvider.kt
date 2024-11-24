8bdd8b349534974db7599dab3c7c36faf01ae5f0
tanwenkang <tanwenkang@kuaishou.com>
2024-11-24 23:49:23 +0800

    Optimize oppo providers' code.

--- library/src/main/java/com/phantomvk/identifier/provider/OppoColorOsProvider.kt
+++ library/src/main/java/com/phantomvk/identifier/provider/OppoColorOsProvider.kt
@@ -2,18 +2,10 @@ package com.phantomvk.identifier.provider
 
 import com.phantomvk.identifier.model.ProviderConfig
 
-internal class OppoColorOsProvider(config: ProviderConfig) : OppoHeyTapProvider(config) {
-
-  override fun isSupported(): Boolean {
-    return isPackageInfoExisted("com.coloros.mcs")
-  }
-
-  override fun run() {
-    getId(
-      "com.oplus.stdid.IStdID",
-      "com.coloros.mcs",
-      "com.oplus.stdid.IdentifyService",
-      "action.com.oplus.stdid.ID_SERVICE"
-    )
-  }
-}
+internal class OppoColorOsProvider(config: ProviderConfig) : OppoHeyTapProvider(
+  config,
+  "com.oplus.stdid.IStdID",
+  "com.coloros.mcs",
+  "com.oplus.stdid.IdentifyService",
+  "action.com.oplus.stdid.ID_SERVICE"
+)
