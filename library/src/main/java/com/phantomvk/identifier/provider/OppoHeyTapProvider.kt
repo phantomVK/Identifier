0cf5e5841a5d9f9e309a6ef6a6e87e84cb588a2f
tanwenkang <tanwenkang@kuaishou.com>
2024-11-27 02:10:58 +0800

    OppoHeyTapProvider has a higher priority than OppoColorOsProvider.

--- library/src/main/java/com/phantomvk/identifier/provider/OppoHeyTapProvider.kt
+++ library/src/main/java/com/phantomvk/identifier/provider/OppoHeyTapProvider.kt
@@ -34,9 +34,9 @@ internal open class OppoHeyTapProvider(
         when (val r = (getId(binder, descriptor, sign, "OAID"))) {
           is BinderResult.Failed -> return r
           is BinderResult.Success -> {
-            val vaid = queryId(IdEnum.VAID) { getId(binder, descriptor, sign, "VAID") }
+//            val vaid = queryId(IdEnum.VAID) { getId(binder, descriptor, sign, "VAID") }
             val aaid = queryId(IdEnum.AAID) { getId(binder, descriptor, sign, "AAID") }
-            return BinderResult.Success(r.id, vaid, aaid)
+            return BinderResult.Success(r.id, null, aaid)
           }
         }
       }
