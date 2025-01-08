d9dbf7df84a4e94c89e2e1d73466e783c5697844
tanwenkang <tanwenkang@kuaishou.com>
2025-01-08 23:36:29 +0800

    Optimize asyncCallback implementation.

--- app/src/androidTest/java/com/phantomvk/identifier/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/phantomvk/identifier/ExampleInstrumentedTest.kt
@@ -1,7 +1,10 @@
 package com.phantomvk.identifier
 
+import android.os.Looper
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.phantomvk.identifier.listener.OnResultListener
+import com.phantomvk.identifier.model.IdentifierResult
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +18,40 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.phantomvk.identifier", appContext.packageName)
-    }
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("com.phantomvk.identifier", appContext.packageName)
+  }
+
+  @Test
+  fun assertCallbackUiThread() {
+    IdentifierManager.build()
+      .enableAsyncCallback(false)
+      .subscribe(object : OnResultListener {
+        override fun onSuccess(result: IdentifierResult) {
+          assertEquals(Looper.getMainLooper(), Looper.myLooper())
+        }
+
+        override fun onError(msg: String, throwable: Throwable?) {
+          assertEquals(Looper.getMainLooper(), Looper.myLooper())
+        }
+      })
+  }
+
+  @Test
+  fun assertCallbackWorkerThread() {
+    IdentifierManager.build()
+      .enableAsyncCallback(true)
+      .subscribe(object : OnResultListener {
+        override fun onSuccess(result: IdentifierResult) {
+          assertNotEquals(Looper.getMainLooper(), Looper.myLooper())
+        }
+
+        override fun onError(msg: String, throwable: Throwable?) {
+          assertNotEquals(Looper.getMainLooper(), Looper.myLooper())
+        }
+      })
+  }
 }
