55ecce7233c58c3f6f436903b627a25fab1fed7c
tanwenkang <tanwenkang@kuaishou.com>
2025-02-28 21:57:49 +0800

    Replace boolean with AtomicBoolean in SerialRunnable.

--- library/src/main/java/com/phantomvk/identifier/impl/SerialRunnable.kt
+++ library/src/main/java/com/phantomvk/identifier/impl/SerialRunnable.kt
@@ -36,13 +36,13 @@ import com.phantomvk.identifier.provider.XiaomiProvider
 import com.phantomvk.identifier.provider.XtcProvider
 import com.phantomvk.identifier.provider.ZteProvider
 import com.phantomvk.identifier.provider.ZuiProvider
+import java.util.concurrent.atomic.AtomicBoolean
 
 internal class SerialRunnable(
   config: ProviderConfig
 ) : AbstractProvider(config), OnResultListener, Disposable {
 
-  @Volatile
-  private var disposed = false
+  private val disposed = AtomicBoolean()
 
   init {
     setCallback(this)
@@ -67,7 +67,7 @@ internal class SerialRunnable(
       return
     }
 
-    if (disposed) {
+    if (disposed.get()) {
       return
     }
 
@@ -116,17 +116,15 @@ internal class SerialRunnable(
   }
 
   override fun isDisposed(): Boolean {
-    return disposed
+    return disposed.get()
   }
 
   private fun invokeCallback(callback: ((OnResultListener) -> Unit)? = null) {
-    if (disposed) {
+    if (disposed.get()) {
       return
     }
 
-    synchronized(this) {
-      if (disposed) return else disposed = true
-
+    if (disposed.compareAndSet(false, true)) {
       if (callback != null) {
         config.callback.get()?.let {
           if (config.asyncCallback && Looper.getMainLooper() == Looper.myLooper()) {
