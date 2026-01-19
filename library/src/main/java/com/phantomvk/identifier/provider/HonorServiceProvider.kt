2005465d09392104a6ced6ef69dbe139804bb211
tanwenkang <tanwenkang@kuaishou.com>
2026-01-19 20:39:15 +0800

    Simplify HonorServiceProvider

--- library/src/main/java/com/phantomvk/identifier/provider/HonorServiceProvider.kt
+++ library/src/main/java/com/phantomvk/identifier/provider/HonorServiceProvider.kt
@@ -15,52 +15,37 @@ internal class HonorServiceProvider(config: ProviderConfig) : AbstractProvider(c
 
   override fun run() {
     val intent = Intent("com.hihonor.id.HnOaIdService").setPackage("com.hihonor.id")
-    bindService(intent) { binder ->
-      if (config.isVerifyLimitAdTracking) {
-        isLimited(binder) { result ->
-          if (result is BinderResult.Success) {
-            getId(binder) { r -> verifyResult(r) }
-          } else {
-            verifyResult(result)
+    bindService(intent) { b -> if (config.isVerifyLimitAdTracking) callBinder(3, b) else callBinder(2, b) }
+  }
+
+  /**
+   * @param code 2:getId(), 3:isLimited()
+   * @param remote binder instance
+   */
+  private fun callBinder(code: Int, remote: IBinder) {
+    val callback = object : IOAIDCallBack.Stub() {
+      override fun a(i: Int, j: Long, z: Boolean, f: Float, d: Double, str: String?) {}
+      override fun onResult(i: Int, bundle: Bundle?) {
+        when (code) {
+          2 -> { // getId()
+            if (i == 0 && bundle != null) {
+              verifyResult(checkId(bundle.getString("oa_id_flag")))
+            } else {
+              getConsumer().onError(BUNDLE_IS_NULL)
+            }
+          }
+
+          3 -> { // isLimited()
+            if (i == 0 && bundle?.getBoolean("oa_id_limit_state") == true) {
+              getConsumer().onError(LIMIT_AD_TRACKING_IS_ENABLED)
+            } else {
+              callBinder(2, remote)
+            }
           }
         }
-      } else {
-        getId(binder) { r -> verifyResult(r) }
-      }
-    }
-  }
-
-  private fun getId(remote: IBinder, callback: OnResultCallback) {
-    val callback = object : IOAIDCallBack.Stub() {
-      override fun a(i: Int, j: Long, z: Boolean, f: Float, d: Double, str: String?) {}
-      override fun onResult(i: Int, bundle: Bundle?) {
-        if (i != 0 || bundle == null) {
-          callback.call(BinderResult.Failed(BUNDLE_IS_NULL))
-        } else {
-          callback.call(checkId(bundle.getString("oa_id_flag")))
-        }
       }
     }
 
-    callBinder(remote, callback, 2)
-  }
-
-  private fun isLimited(remote: IBinder, callback: OnResultCallback) {
-    val callback = object : IOAIDCallBack.Stub() {
-      override fun a(i: Int, j: Long, z: Boolean, f: Float, d: Double, str: String?) {}
-      override fun onResult(i: Int, bundle: Bundle?) {
-        if (i == 0 && bundle?.getBoolean("oa_id_limit_state") == true) {
-          callback.call(BinderResult.Failed(LIMIT_AD_TRACKING_IS_ENABLED))
-        } else {
-          callback.call(BinderResult.Success("", "", ""))
-        }
-      }
-    }
-
-    callBinder(remote, callback, 3)
-  }
-
-  private fun callBinder(remote: IBinder, callback: IOAIDCallBack, code: Int) {
     val data = Parcel.obtain()
     val reply = Parcel.obtain()
     try {
@@ -69,13 +54,10 @@ internal class HonorServiceProvider(config: ProviderConfig) : AbstractProvider(c
       remote.transact(code, data, reply, 0)
       reply.readException()
     } catch (t: Throwable) {
+      getConsumer().onError(EXCEPTION_THROWN, t)
     } finally {
       reply.recycle()
       data.recycle()
     }
-  }
-
-  private fun interface OnResultCallback {
-    fun call(result: BinderResult)
   }
 }
